1d879845c3beed9d3bdcc3d854c4264581dedc71
Jovansonlee Cesar <ivanceras@gmail.com>
2018-01-24 13:54:39 +0800

    Create DB first

--- data/reimport.sh
+++ data/reimport.sh
@@ -1,3 +1,4 @@
+psql -U postgres -h localhost -c 'CREATE DATABASE dota;' 
 psql -U postgres -d dota -h localhost -c "DROP SCHEMA public cascade; CREATE SCHEMA public;"
 psql -U postgres -d dota -h localhost -f schema.sql
 psql -U postgres -d dota -h localhost -f data-heroes.sql
